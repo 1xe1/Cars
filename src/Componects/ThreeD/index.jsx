be4b0a1ef83ee0540e20709f7ebdd374d55f932d
1xe1 <641463021@crru.ac.th>
2023-08-22 09:12:29 +0700

    a

--- src/Componects/ThreeD/index.jsx
+++ src/Componects/ThreeD/index.jsx
@@ -3,10 +3,15 @@ import "./ThreeD.css";
 
 function ThreeD() {
   return (
-    <div className="box">
-      <span style={{ "--i": 1 }}>
-        <img src="https://topicstock.pantip.com/ratchada/topicstock/2008/11/V7162667/V7162667-0.jpg" alt="" />
-      </span>
+    <div className="flex justify-center items-center h-screen">
+      <div className="box">
+        <span style={{ "--i": 1 }}>
+          <img
+            src="https://topicstock.pantip.com/ratchada/topicstock/2008/11/V7162667/V7162667-0.jpg"
+            alt=""
+          />
+        </span>
+      </div>
     </div>
   );
 }
